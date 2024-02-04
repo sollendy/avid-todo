f0fb746cb0ac4cefa6d230525ff2bab0ee66e213
sollendy <edlannino@tutanota.com>
2024-02-04 17:54:56 +0100

    todo completata.

--- js/script.js
+++ js/script.js
@@ -3,30 +3,27 @@ createApp({
 
     data() {
         return {
-            agenda: [
-                {
-                    text: "andare dal dottore",
-                    done: false,
-                },
-
-                {
-                    text: "fare una ricarica telefonica",
-                    done: true,
-                },
-
-                {
-                    text: "scrivere il resto delle commissioni tramite il bottone",
-                    done: false,
-                },
-                
-            ],
+            agenda: this.loadFromLocalStorage() || [
+        {
+          text: "andare dal dottore",
+          done: false,
+        },
+        {
+          text: "fare una ricarica telefonica",
+          done: true,
+        },
+        {
+          text: "scrivere il resto delle commissioni tramite il bottone",
+          done: false,
+        },
+      ],
 
             index: 0,
             newLi: "",
             
         }
     },
-    
+
     methods: {
 
         //  changeDone() {
@@ -41,20 +38,28 @@ createApp({
 
         deleteLi(index) {
             this.agenda.splice(index, 1);
-        },
-
-        addLi(){
+            this.saveToLocalStorage();
+          },
+          addLi() {
             const newTodo = {
-                text: this.newLi,
-                done: false
-            }
+              text: this.newLi,
+              done: false,
+            };
             this.agenda.push(newTodo);
             this.newLi = "";
-        },
-
-        doneLi(index){
+            this.saveToLocalStorage();
+          },
+          doneLi(index) {
             this.agenda[index].done = true;
-        }
+            this.saveToLocalStorage();
+          },
+          saveToLocalStorage() {
+            localStorage.setItem("agenda", JSON.stringify(this.agenda));
+          },
+          loadFromLocalStorage() {
+            const storedAgenda = localStorage.getItem("agenda");
+            return storedAgenda ? JSON.parse(storedAgenda) : this.agenda;
+          },
 
     }
 
